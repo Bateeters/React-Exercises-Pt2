baa37d47a62f19b594cb42419dde9fd260b94e31
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-08-11 19:36:06 -0400

    Completed First React Exercise: Counter Component

---
+++ React Exercises Pt2/src/App.jsx
@@ -0,0 +1,21 @@
+import Counter from './components/Counter'
+import './css/App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h3>Welcome to my collection of React Exercises!</h3>
+      <hr />
+      <h2>Beginner React Exercises</h2>
+      <Counter/>
+      <hr />
+      <h2>Intermediate React Exercises</h2>
+      <hr />
+      <h2>Advanced React Exercises</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ React Exercises Pt2/src/components/Counter.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+
+function Counter() {
+    let [counter, setCounter] = useState(0)
+    let [oddOrEven, setOddOrEven] = useState(null)
+
+    const increaseCount = () => {
+        setCounter(++counter)
+        oddOrEvenNumber(counter)
+    }
+
+    const decreaseCount = () => {
+        setCounter(--counter)
+        oddOrEvenNumber(counter)
+    }
+
+    const resetCount = () => {
+        setCounter(0)
+        setOddOrEven(null)
+    }
+
+    function oddOrEvenNumber(counter) {
+        if (counter === 0) {
+            setOddOrEven(null)
+        } else {
+            if (counter%2 === 1) {
+                setOddOrEven("Odd")
+            } else {
+                setOddOrEven("Even")
+            }
+        }
+    }
+
+    return (
+        <div>
+            <h1>1. Counter Component</h1>
+            <h3>Goal: Create a Simple counter with increase, decrease, and reset functionality.</h3>
+            <p>Steps:</p>
+            <ol>
+                <li>Create a new component called Counter.</li>
+                <li>Initialize a count state variable using useState(0)</li>
+                <li>Render the current count.</li>
+                <li>Add three buttons: "Increase", "Decrease", and "Reset."</li>
+                <li>Attach onClick handlers to update the state accordingly.</li>
+                <li>Optional: Display whether the number is even or odd.</li>
+            </ol>
+            <p>Exercise Solution:</p>
+            <p>{counter} : {oddOrEven}</p>
+            <button onClick={increaseCount}>Increase</button>
+            <button onClick={decreaseCount}>Decrease</button>
+            <button onClick={resetCount}>Reset</button>
+            
+        </div>
+    )
+}
+
+export default Counter
