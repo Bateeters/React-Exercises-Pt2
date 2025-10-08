787c07933a81135747ccf70a84e891cd4e998c26
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-10-08 16:44:00 -0400

    updated comments in ListGroup

--- React Exercises Pt2/src/components/ListGroup.jsx
+++ React Exercises Pt2/src/components/ListGroup.jsx
@@ -1,24 +1,21 @@
 import { useState } from "react";
 
-function ListGroup({items, heading}) {
+function ListGroup({items, heading}) { // define the props, or arguments if thinking in terms of functions, needed for component
+    
     // initialize our state and start it at “-1” so none of the items are highlighted 
     // (0 would highlight the first as it goes by index)
     const [selectedIndex, setSelectedIndex] = useState(-1);  
+    
     return (
-	    <>
-		    <h3>{heading}</h3>
+	    <> { // use the passed "heading" and "items" to dynamically render/display
+            }
+		    <h3>{heading}</h3> 
 		    {items.length === 0 && <p>No item found</p>}
             <ul className='list-group'>
                 {items.map((item, id) => (
                     <li key={id} 
-
-                    // check to see what the selectedIndex state is, 
-                    // if it matches the element’s id, apply the “highlight” class, 
-                    // if not, just “list-item” class
-                    className={ selectedIndex === id ? 'list-item highlight' : 'list-item'}    
-
-                    // on click, update selectedIndex state to the element’s id
-                    onClick={() => {setSelectedIndex(id)}} 
+                        className={ selectedIndex === id ? 'list-item highlight' : 'list-item'}    
+                        onClick={() => {setSelectedIndex(id)}} 
                     >
                         {item}
                     </li>
