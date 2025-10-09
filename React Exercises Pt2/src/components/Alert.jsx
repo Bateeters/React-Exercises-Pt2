f8e867af6b5c9283989998c5e8547c05f449e559
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-10-09 17:50:52 -0400

    added comments explaining the Alert component

--- React Exercises Pt2/src/components/Alert.jsx
+++ React Exercises Pt2/src/components/Alert.jsx
@@ -1,18 +1,20 @@
-function Alert({type = "info", children}) {
-    const colors = {
+function Alert({type = "info", children}) { // initializing the component, using "type" to determine what color the Alert shows as
+    const colors = { // List of instances the alert can be used as with matching colors.
         info: "lightblue",
         warning: "goldenrod",
         danger: "tomato"
     };
 
     return (
-        <div style={{
-            backgroundColor: colors[type],
+        <div style={{ // styling the div the Alert is shown in
+            backgroundColor: colors[type], // this is where we utilize the "type" prop to determine color
             color: "white",
             padding: "10px",
             borderRadius: "15px"
         }}>
-            {children}
+            {// we pull in all the content between the Alert open and closing tags (text and HTML tags).
+            }
+            {children} 
         </div>
     )
 }
